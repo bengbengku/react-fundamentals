3ce0e2214b7fea8837e4e31478dd8eab223ab2e9
bengbengku <anggiatbenget70@gmail.com>
2020-11-20 14:55:48 +0700

    React Fundamentals

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//CSS
+import "./index.css";
+//Setup vars
+const firstBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+  title: "I love you to the moon and back",
+  author: "Amelia Hepworth",
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const secondBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg",
+  title: "Our Class is a Family",
+  author: "Sandie Sonkie",
+};
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia est
+          facere molestias fugit, aliquid consequatur. Nulla incidunt expedita
+          quos. Ea necessitatibus facere architecto nihil aliquam atque. Fuga ea
+          aperiam nesciunt.
+        </p>
+      </Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+    </section>
+  );
+}
+
+const Book = (props) => {
+  // Bisa juga (props) lalu panggil props.img etc.
+  const {img, title, author, children} = props
+  return (
+    <article className="book">
+      <img src={img} alt="Book Cover" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {children}
+    </article>
+  );
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
